efc379a9933248c039d423930bbf8090a62256ab
Adam Fischbach <fischbachadam92@gmail.com>
2018-08-04 16:28:57 +0200

    add bdo object

---
+++ src/main/java/bdo/PhoneCover.java
@@ -0,0 +1,23 @@
+package bdo;
+
+public class PhoneCover {
+
+    private String phoneName;
+    private String motive;
+
+    public String getPhoneName() {
+        return phoneName;
+    }
+
+    public String getMotive() {
+        return motive;
+    }
+
+    public void setPhoneName(String phoneName) {
+        this.phoneName = phoneName;
+    }
+
+    public void setMotive(String motive) {
+        this.motive = motive;
+    }
+}
