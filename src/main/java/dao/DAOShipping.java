71be4b942e96af0432bb0b31461c2677be755773
Adam Fischbach <fischbachadam92@gmail.com>
2018-08-12 14:54:29 +0200

    add presentation of shipping options

---
+++ src/main/java/dao/DAOShipping.java
@@ -0,0 +1,38 @@
+package dao;
+
+import data.DbConnectionSingletonFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DAOShipping {
+
+    public ObservableList<String> getShippingOption() throws SQLException {
+        ObservableList<String> shippingOptiions = FXCollections.observableArrayList();
+
+        String sqlQuery = "SELECT option "+
+                "FROM ShippingOption ";
+
+
+        Connection con = DbConnectionSingletonFactory.getConnection();
+
+        Statement stmt = null;
+        stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        while (rs.next()){
+            String option = rs.getString("option");
+            shippingOptiions.add(option);
+        }
+
+        return shippingOptiions;
+
+
+    }
+
+
+
+}
